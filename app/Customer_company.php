a2197105d1c893176c8c989a17f4bed506f1d824
Mohaiminus Sakib <rode@scraperite.com>
2017-05-30 14:47:17 +0600

    Companies migrations and seeders implemented

---
+++ app/Customer_company.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer_company extends Model
+{
+    public function employees(){
+        return $this->hasMany('App\Customer');
+    }
+
+    public function address(){
+        return $this->hasManyThrough('App\Address', 'App\Customer');
+    }
+
+
+}
