588ef9d10e478227d049db75134350d4fbfc48f3
Mohaiminus Sakib <rode@scraperite.com>
2017-06-05 15:09:04 +0600

    Added Company view page with tabs

---
+++ database/migrations/2017_06_05_090510_create_journals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJournalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journals', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->enum('status', ['Due', 'Done', 'Cancelled']);
+            $table->string('priority', ['Critical', 'High', 'Medium', 'Low']);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journals');
+    }
+}
---
+++ database/migrations/2017_06_05_090711_create_appointments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
