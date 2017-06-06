46d9c4fa0cf2fe21a92100ee6c0802857321e7ed
Mohaiminus Sakib <rode@scraperite.com>
2017-06-06 21:16:59 +0600

    Added employee list

--- resources/views/customer-company/view.blade.php
+++ resources/views/customer-company/view.blade.php
@@ -109,13 +109,13 @@
                                                 <h3 class="panel-title">Tasks</h3>
                                             </div>
                                             <div class="panel-body">
-                                                <div class="panel-group panel-group-theme-1" id="accordion-2" role="tablist" aria-multiselectable="true">
+                                                <div class="panel-group panel-group-theme-1" id="tasklist" role="tablist" aria-multiselectable="true">
                                                     <div class="panel panel-default">
-                                                        <div class="panel-heading panel-heading-theme-1" role="tab" id="headingOne-2">
-                                                            <h4 class="panel-title"><a class="active collapsed" data-toggle="collapse" data-parent="#accordion-2" href="#collapseOne-2" aria-expanded="false" aria-controls="collapseOne-2"><i class="fa fa-plus-square"></i> Collapsible Group Item #1</a></h4>
+                                                        <div class="panel-heading panel-heading-theme-1" role="tab" id="task-heading-1">
+                                                            <h4 class="panel-title"><a class="active collapsed" data-toggle="collapse" data-parent="#tasklist" href="#task-1" aria-expanded="false" aria-controls="task-1"><i class="fa fa-plus-square"></i> Collapsible Group Item #1</a></h4>
                                                         </div>
 
-                                                        <div id="collapseOne-2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne-2" aria-expanded="false" style="height: 0px;">
+                                                        <div id="task-1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="task-heading-1" aria-expanded="false" style="height: 0px;">
                                                             <div class="panel-body">
                                                                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                                                             </div>
@@ -124,7 +124,7 @@
 
                                                     <div class="panel panel-default">
                                                         <div class="panel-heading panel-heading-theme-1" role="tab" id="headingTwo-2">
-                                                            <h4 class="panel-title"><a class="" data-toggle="collapse" data-parent="#accordion-2" href="#collapseTwo-2" aria-expanded="true" aria-controls="collapseTwo-2"><i class="fa fa-minus-square"></i> Collapsible Group Item #2</a></h4>
+                                                            <h4 class="panel-title"><a class="" data-toggle="collapse" data-parent="#tasklist" href="#collapseTwo-2" aria-expanded="true" aria-controls="collapseTwo-2"><i class="fa fa-minus-square"></i> Collapsible Group Item #2</a></h4>
                                                         </div>
 
                                                         <div id="collapseTwo-2" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo-2" aria-expanded="true" style="">
@@ -136,7 +136,7 @@
 
                                                     <div class="panel panel-default">
                                                         <div class="panel-heading panel-heading-theme-1" role="tab" id="headingThree-2">
-                                                            <h4 class="panel-title"><a class="collapsed" data-toggle="collapse" data-parent="#accordion-2" href="#collapseThree-2" aria-expanded="false" aria-controls="collapseThree-2"><i class="fa fa-plus-square"></i> Collapsible Group Item #3</a></h4>
+                                                            <h4 class="panel-title"><a class="collapsed" data-toggle="collapse" data-parent="#tasklist" href="#collapseThree-2" aria-expanded="false" aria-controls="collapseThree-2"><i class="fa fa-plus-square"></i> Collapsible Group Item #3</a></h4>
                                                         </div>
 
                                                         <div id="collapseThree-2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree-2" aria-expanded="false">
@@ -250,7 +250,28 @@
                                                 <h3 class="panel-title">Contact Persons</h3>
                                             </div>
                                             <div class="panel-body">
-
+                                                <div class="table-responsive">
+                                                    <table class="table table-bordered">
+                                                        <thead>
+                                                        <tr>
+                                                            <th>Name</th>
+                                                            <th>Role</th>
+                                                            <th>Location</th>
+                                                            <th>Actions</th>
+                                                        </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                        @foreach($company->employees as $employee)
+                                                            <tr>
+                                                                <td>{{implode(', ', array_filter([$employee->last_name, $employee->first_name]))}}</td>
+                                                                <td>{{$employee->title}}</td>
+                                                                <td>{{implode(', ', array_filter([$employee->addresses->first()->city, $employee->addresses->first()->state, $employee->addresses->first()->country, $employee->addresses->first()->zip]))}}</td>
+                                                                <td><a href="#" class="btn btn-success">View</a></td>
+                                                            </tr>
+                                                        @endforeach
+                                                        </tbody>
+                                                    </table>
+                                                </div>
                                             </div>
                                         </div>
 
