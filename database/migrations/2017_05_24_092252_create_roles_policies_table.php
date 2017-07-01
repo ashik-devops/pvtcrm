dfba114348d1883fa4bafb4318642a6aaee93e60
Mohaiminus Sakib <rode@scraperite.com>
2017-07-02 04:59:04 +0600

    Updated policy migrations and seeders

--- database/migrations/2017_05_24_092252_create_roles_policies_table.php
+++ database/migrations/2017_05_24_092252_create_roles_policies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersPoliciesTable extends Migration
+class CreateRolesPoliciesTable extends Migration
 {
     /**
      * Run the migrations.
