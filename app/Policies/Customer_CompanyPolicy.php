7f038f8844b07029d521b35f47e779b659666210
Mohaiminus Sakib <rode@scraperite.com>
2017-07-03 22:43:25 +0600

    Added Company Policy but needs edit

---
+++ app/Policies/Customer_CompanyPolicy.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Policies;
+
+use App\Customer_company;
+use App\Traits\AdminPolicies;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class Customer_CompanyPolicy
+{
+    use HandlesAuthorization, AdminPolicies;
+
+    /**
+     * Determine whether the user can list all customers.
+     *
+     * @param  \App\User  $authenticated_user
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function index(User $user)
+    {
+        if($this->checkAdmin($user)){
+            return true;
+        }
+        return !is_null($user->policies()->whereIn('scope', ['*', 'customer'])
+            ->whereIn('action',['*','list'])->first()->id);
+    }
+
+    /**
+     * Determine whether the user can view the customer.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Customer  $company
+     * @return mixed
+     */
+    public function view(User $user, Customer_company $company)
+    {
+        if($this->checkAdmin($user)){
+            return true;
+        }
+        return $user->id === $company->user->id || !is_null($user->policies()->whereIn('scope', ['*', 'customer'])
+                ->whereIn('action',['*','view'])->first()->id);
+
+    }
+
+    /**
+     * Determine whether the user can create customers.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        if($this->checkAdmin($user)){
+            return true;
+        }
+        return  !is_null($user->policies()->whereIn('scope', ['*', 'customer'])
+            ->whereIn('action',['*','create'])->first()->id);
+    }
+
+    /**
+     * Determine whether the user can update the customer.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Customer  $company
+     * @return mixed
+     */
+    public function update(User $user, Customer_company $company)
+    {
+        if($this->checkAdmin($user)){
+            return true;
+        }
+        return $user->id === $company->user->id || !is_null($user->policies()->whereIn('scope', ['*', 'customer'])
+                ->whereIn('action',['*','edit'])->first()->id);
+    }
+
+    /**
+     * Determine whether the user can delete the customer.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Customer  $company
+     * @return mixed
+     */
+    public function delete(User $user, Customer_company $company)
+    {
+        if($this->checkAdmin($user)){
+            return true;
+        }
+        return $user->id === $company->user->id || !is_null($user->policies()->whereIn('scope', ['*', 'customer'])
+                ->whereIn('action',['*','delete'])->first()->id);
+    }
+}
