4eca08c11cea19aa3f273af50853a8421d4c4272
Mohaiminus Sakib <rode@scraperite.com>
2017-07-14 06:53:20 +0600

    Updated migration and controller

---
+++ database/migrations/2017_07_14_004244_create_view_appointment_index.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateViewAppointmentIndex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Illuminate\Support\Facades\DB::statement("create VIEW appointments_index AS select `appointments`.`id`, `appointments`.`title`, `appointments`.`description`, `appointments`.`start_time`, `appointments`.`end_time`, `appointments`.`status`, customer.last_name, customer.first_name, company.name, customer.id AS customer_id, company.id AS company_id, CONCAT(CONCAT(customer.last_name, ',', customer.first_name),'@', company.name ) AS customer_name from `appointments` inner join `customers` as `customer` on `appointments`.`customer_id` = `customer`.`id` inner join `customer_companies` as `company` on `customer`.`customer_company_id` = `company`.`id` where `appointments`.`deleted_at` is null");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        \Illuminate\Support\Facades\DB::statement("DROP VIEW appointments_index");
+    }
+}
