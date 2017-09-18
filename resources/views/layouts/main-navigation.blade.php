cfc0a82e520ba658c986a4b4332f8e2d110762e5
Tahmid <tuhi009@live.com>
2017-09-18 02:21:56 -0700

    Fixed active navbar

--- resources/views/layouts/main-navigation.blade.php
+++ resources/views/layouts/main-navigation.blade.php
@@ -2,45 +2,45 @@
 <div class="main-nav-wrapper">
     <nav id="main-nav" class="main-nav js-cloak">
         <ul id="menu">
-            <li  {{ Request::is('/') ? ' class="active"' : null }}>
+            <li class="{{Nav::isRoute('dashboard',"active")}}">
                 <a href="/">
                     <span aria-hidden="true" class="icon icon_house_alt"></span>
                     <span class="nav-label">Dashboard</span>
                 </a>
             </li>
 
-            <li {{ Request::is('calendar') ? ' class="active"' : null }}>
+            <li class="{{Nav::isRoute('calendar',"active")}}" >
                 <a href="{{route('calendar')}}">
                     <span aria-hidden="true" class="fa fa-calendar"></span>
                     <span class="nav-label">Calendar</span>
                 </a>
             </li>
-            <li {{ Request::is('accounts') ? ' class="active"' : null }}>
+            <li class="{{Nav::isRoute('account-index',"active")}}">
                 <a href="{{route('account-index')}}">
                     <span aria-hidden="true" class="icon icon_building_alt"></span>
                     <span class="nav-label">Accounts</span>
                 </a>
             </li>
-            <li {{ Request::is('customers') ? ' class="active"' : null }}>
+            <li class="{{Nav::isRoute('customer-index',"active")}}">
                 <a href="{{route('customer-index')}}">
                     <span aria-hidden="true" class="icon icon_group"></span>
                     <span class="nav-label">Customers</span>
                 </a>
             </li>
-            <li {{ Request::is('task-index') ? ' class="active"' : null }}>
+            <li class="{{Nav::isRoute('task-index',"active")}}">
                 <a href="{{route('task-index')}}">
                     <span aria-hidden="true" class="fa fa-tasks"></span>
                     <span class="nav-label">Tasks</span>
                 </a>
             </li>
-            <li {{ Request::is('appointment-index') ? ' class="active"' : null }}>
+            <li class="{{Nav::isRoute('appointment-index',"active")}}">
                 <a href="{{route('appointment-index')}}">
                     <span aria-hidden="true" class="fa fa-calendar-check-o"></span>
                     <span class="nav-label">Appointments</span>
                 </a>
             </li>
             @can('index', \App\User::class)
-            <li {{ Request::is('users') ? ' class="active"' : null }}>
+            <li class="{{Nav::isRoute('users-index',"active")}}">
                 <a href="{{route('users-index')}}">
                     <span aria-hidden="true" class="icon icon_group"></span>
                     <span class="nav-label">Users</span>
