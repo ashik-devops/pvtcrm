d8fb9c88d4e15bd1807e95a529a895175aa77903
Rode <rode@pvtcrm.com>
2017-10-08 16:04:36 +0600

    - Fixed bug related to sidebar activity list - Added access control to activity data provider APIs - Updated API response by removing unnecessary data

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Appointment;
+use App\Customer;
+use App\Task;
 use App\Traits\PolicyHelpers;
 use App\User;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Activitylog\Models\Activity;
@@ -26,18 +30,8 @@ class ActivityController extends Controller
     }
 
     public function getActivities(Request $request){
-        $from = is_null($request->from)?Carbon::today()->firstOfMonth()->startOfDay():Carbon::createFromFormat('m/d/Y H:i A', $request->from);
-        $to = is_null($request->to)?Carbon::today()->endOfDay():Carbon::createFromFormat('m/d/Y H:i A', $request->to);
 
-        $activities =  Activity::whereBetween('created_at', [$from, $to]);
-
-        $activities = $this->applyUserFIltering($activities, $request->user);
-
-
-        if(!is_null($request->type)){
-            $activities = $activities->where('description', '=', $request->type);
-        }
-
+        $activities = $this->applyRequestFilters($request);
         $activities = $activities->select('description', 'created_at');
 
         return DataTables::of($activities)
@@ -49,7 +43,7 @@ class ActivityController extends Controller
             ->make();
     }
     public function recentActivities($count=10){
-        return response()->json($this->applyUserFIltering(Activity::with('causer', 'subject'))->orderBy('created_at', 'desc')->take($count)->get()->map(function($activity){
+        return response()->json($this->applyUserFilters(Activity::with('causer', 'subject'))->orderBy('created_at', 'desc')->take($count)->get()->map(function($activity){
 
             $date =  new Carbon($activity->created_at);
             $log = new \stdClass();
@@ -61,22 +55,30 @@ class ActivityController extends Controller
         }), 200);
     }
 
-    function getLogMessage(Activity $activity): string
-    {
-        if (is_null($activity->causer)) {
-            return "<strong><a href='#'> System</a></strong> has {$activity->description} {$activity->subject->obj_alias}: <a href='".$activity->subject->getLink()."'>".$activity->subject->getActivityTitle()."</a>";
-        }
 
-        return'<strong><a href="' . route('profile-view', [$activity->causer->id]) . '">' . $activity->causer->name . '</a><strong> has ' .$activity->description . ' ' . $activity->subject->obj_alias . ': <a href="' . $activity->subject->getLink() . '">' . $activity->subject->getActivityTitle() . '</a>';
+    public function customerActivity(Request $request, Customer $customer){
+
+        $activities = $this->applyRequestFilters($request);
+        $activities = $activities->select('description', 'created_at');
+
+        return DataTables::of($activities)
+            ->addColumn('created_at', function ($activity){
+                $date =  new Carbon($activity->created_at);
+                return $date->toFormattedDateString();
+            })
+            ->rawColumns(['description'])
+            ->make();
 
     }
 
     /**
+     * Applies filter according to user scope
+     *
      * @param Request $request
      * @param $activities
-     * @return mixed
+     * @return \Illuminate\Database\Eloquent\Builder
      */
-    private function applyUserFIltering($activities, $user=null)
+    private function applyUserFilters($activities, $user=null): Builder
     {
         if ($this->checkAdmin(Auth::user())) {
             if (!is_null($user)) {
@@ -89,6 +91,31 @@ class ActivityController extends Controller
         } else {
             $activities = $activities->where('causer_id', 'IN', Auth::user()->getSubordinates());
         }
+        return $activities;
+    }
+
+    /**
+     * Applies request filters
+     *
+     * @param Request $request
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    private function applyRequestFilters(Request $request): Builder
+    {
+        $from = is_null($request->from) ? Carbon::today()->firstOfMonth()->startOfDay() : Carbon::createFromFormat('m/d/Y H:i A', $request->from);
+        $to = is_null($request->to) ? Carbon::today()->endOfDay() : Carbon::createFromFormat('m/d/Y H:i A', $request->to);
+
+        $activities = Activity::whereBetween('created_at', [$from, $to]);
+
+        $activities = $this->applyUserFilters($activities, $request->user);
+
+
+        if (!is_null($request->type)) {
+            $activities = $activities->where('description', '=', $request->type);
+        }
+
+        dd(get_class($activities));
+
         return $activities;
     }
 }
