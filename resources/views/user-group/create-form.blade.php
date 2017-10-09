e257a6ba23a74c3ff62d6a0af75302c9df286d08
Tahmid <tuhi009@live.com>
2017-10-09 12:06:43 +0600

    - linked modal in index

--- resources/views/user-group/create-form.blade.php
+++ resources/views/user-group/create-form.blade.php
@@ -1,4 +1,4 @@
-@section('usergroup-create-form')
+@section('appointment-create-form')
     <form method="post" class="ajax-from"  data-parsley-validate id="userGroupForm">
 
         {{ csrf_field() }}
