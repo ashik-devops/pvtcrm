30d695927c17339e2335a3a1b4938da8e69985a3
Rode <rode@pvtcrm.com>
2017-10-10 14:27:37 +0600

    creae working

--- app/Http/Controllers/UsergroupController.php
+++ app/Http/Controllers/UsergroupController.php
@@ -2,15 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Customer;
-use App\Index_appointment;
-use App\Journal;
-use App\Task;
-use App\User;
+
 use App\UserGroup;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Yajra\DataTables\DataTables;
